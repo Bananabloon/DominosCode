c437b6a86a03c61115512da9458aa69176172876
Bananabloon <99277409+Bananabloon@users.noreply.github.com>
2025-01-07 13:23:55 +0100

    Update TempMailGet.py

--- TempMailGet.py
+++ TempMailGet.py
@@ -2,3 +2,8 @@
 #https://api.mail.tm/
 #generalnie to narazie nie mam nic, nie instaluje ani python ani pipa zakładam, że użytkownik to już ma,
 #tylko, że te jebane kompy tego nie mają
+
+import requests
+
+response = requests.get('https://api.mail.tm/domains', verify=True)
+print(response.json())
